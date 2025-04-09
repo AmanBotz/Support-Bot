340f1334d624388d6ca36649df87e54bf60eb368
Aman Godz <amanpooniaff@gmail.com>
2025-04-09 10:39:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,16 @@
 from flask import Flask
 from main import app as bot_client
 import threading
-import os
 
 web = Flask(__name__)
 
 @web.route("/")
 def home():
-    return "Support Bot Running"
+    return "Bot Running"
 
 def run_bot():
-    bot_client.start()
-    print("Bot client started")
     bot_client.run()
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
-    web.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    web.run(host="0.0.0.0", port=8000)
