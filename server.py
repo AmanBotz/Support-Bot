6943cf9e819833336310b016526de52ed157072f
Aman Godz <amanpooniaff@gmail.com>
2025-04-09 08:54:41 +0530

    Update and rename flask.py to server.py

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from main import app as bot_client
 import threading
+import os
 
-app = Flask(__name__)
+web = Flask(__name__)
 
-@app.route("/")
+@web.route("/")
 def home():
     return "Support Bot Running"
 
 def run_bot():
+    bot_client.start()
+    print("Bot client started")
     bot_client.run()
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
-    app.run(host="0.0.0.0", port=8000)
+    web.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
